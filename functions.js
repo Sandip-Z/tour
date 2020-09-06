25670c3dc82f131526322dd57efdfb36eba22865
sandip satyal <sandipsatyal25@gmail.com>
2020-09-06 22:03:30 +0545

    [add] change gallery photo based on location clicked

--- functions.js
+++ functions.js
@@ -2,17 +2,28 @@ const nav = document.querySelector("nav");
 window.onscroll = function (e) {
   if (window.pageYOffset > 119) {
     nav.style.backgroundColor = "#363737";
+    nav.classList.add("shadow");
   } else {
     nav.style.backgroundColor = "transparent";
+    nav.classList.remove("shadow");
   }
 };
 
 const galleryMenu = document.querySelectorAll(".gallery-menu-list");
+const galleryImages = document.querySelectorAll(".gallery-img");
 galleryMenu.forEach((list) => {
   list.onclick = (e) => {
+    reflectOnGallery(e.target.id);
     galleryMenu.forEach((node) => {
       node.classList.remove("active");
     });
     list.classList.add("active");
   };
 });
+
+function reflectOnGallery(id) {
+  galleryImages.forEach((image) => {
+    const newSrc = image.src.replace(image.src.split("-")[2], `${id}.jpg`);
+    image.setAttribute("src", newSrc);
+  });
+}
