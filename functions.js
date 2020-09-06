9b889ada28a872a411ca47d76ded349719d8b0d5
sandip satyal <sandipsatyal25@gmail.com>
2020-09-06 20:53:30 +0545

    [change] navbar bg-color

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector("nav");
 window.onscroll = function (e) {
   if (window.pageYOffset > 119) {
-    nav.style.backgroundColor = "#2c3e50";
+    nav.style.backgroundColor = "#363737";
   } else {
     nav.style.backgroundColor = "transparent";
   }
